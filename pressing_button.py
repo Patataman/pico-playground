112ca95b95f845801f1783a48b9388f0814e0bb8
Patataman <shas_arroyo@hotmail.com>
2021-08-05 11:09:54 +0200

    use button input

---
+++ pressing_button.py
@@ -0,0 +1,22 @@
+from machine import Pin
+import time
+
+LED = Pin(25, Pin.OUT)
+DELAY = time.ticks_ms()
+
+def alter_led(inst):
+    global LED, DELAY
+    
+    if time.ticks_diff(time.ticks_ms(), DELAY) < 500:
+        return
+    
+    print(inst.value)
+    LED.toggle()
+    print("Press button works!")
+    DELAY = time.ticks_ms()
+
+but = Pin(0, Pin.IN, Pin.PULL_UP)
+but.irq(handler=alter_led, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
+
+
+LED.value(0)
